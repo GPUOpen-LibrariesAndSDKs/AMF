43b5211be2d84056ea940b50a98758ec369afef6
tnarine-amd <tnarine@amd.com>
2017-05-01 12:52:37 -0400

    Fix
    The byteOffset ref param is now reset after the test.

--- amf/public/samples/CPPSamples/common/AudioPresenter.cpp
+++ amf/public/samples/CPPSamples/common/AudioPresenter.cpp
@@ -57,7 +57,6 @@ AMF_RESULT AudioPresenter::Seek(amf_pts pts)
 bool AudioPresenter::HandleSeek(const AMFAudioBuffer* pBuffer, bool& bDiscard, amf_size& byteOffset)
 {
     bDiscard = false;
-    byteOffset = 0;
 
     amf::AMFLock lock(&m_cs);
 
@@ -65,6 +64,8 @@ bool AudioPresenter::HandleSeek(const AMFAudioBuffer* pBuffer, bool& bDiscard, a
     {
         return false;
     }
+
+    byteOffset = 0;
 
     const amf_pts
         pts = const_cast<AMFAudioBuffer*>(pBuffer)->GetPts(),
